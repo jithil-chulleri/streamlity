ad0f119b74121c8c53af28d76277cea70d4f3112
jithil-chulleri <jithilc4@gmail.com>
2024-11-02 09:07:49 +0530

    test

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.title("Simple Streamlit App")
-data = {"Name": ["Alice", "Bob", "Charlie"], "Age": [25, 30, 35]}
+data = {"Name": ["Jithil", "Bob", "Charlie"], "Age": [25, 30, 35]}
 df = pd.DataFrame(data)
 st.write("Here's a sample DataFrame:")
 st.write(df)
+
